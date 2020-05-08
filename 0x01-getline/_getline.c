703976d677da951e0f938fe8182b0b9c4d23c032
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-05-08 14:43:53 -0500

    (0x01) feat: getline implementation
    Created a simple implementation for the `getline` function. This
    implementation will return a line from a file descriptor. If the
    function is called various times with the same file, it will return
    the next line until eventually, all the contents of the file are read.
    In that case the function will return a null pointer.

---
+++ 0x01-getline/_getline.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include "_getline.h"
+
+
+/**
+ * _getline - retrieves a line from a file.
+ *
+ *  _getline will return a line from a file descriptor. If the
+ * function is called various times with the same file, it will return
+ * the next line until eventually, all the contents of the file are read.
+ * In that case the function will return a null pointer.
+ *
+ * The obtained line has to be free'd when it is no longer in use.
+ *
+ * @fd: file descriptor.
+ *
+ * Return: string duplicate of the obtained line.
+ */
+char *_getline(const int fd)
+{
+	static int file, i;
+	static char buf[READ_SIZE];
+	int j, b = 0;
+	char line[READ_SIZE];
+
+	if (file == 0)
+	{
+		file = fd;
+		memset(buf, '\0', READ_SIZE);
+	}
+	if (file != fd)
+		return (NULL);
+	memset(line, '\0', READ_SIZE);
+	if (!buf[i])
+	{
+		if (read(file, buf, READ_SIZE) == 0)
+			return (NULL);
+
+	}
+	j = i;
+
+	while (buf[i])
+	{
+		if (buf[j] != '\n')
+		{
+			j++;
+			b++;
+		}
+		else
+			break;
+	}
+	strncpy(line, &buf[i], b);
+	i = j + 1;
+
+	return (strdup(line));
+}
---
+++ 0x01-getline/_getline.h
@@ -0,0 +1,8 @@
+#ifndef GETLINE
+#define GETLINE
+
+#define READ_SIZE 1024
+
+char *_getline(const int fd);
+
+#endif /* ifndef GETLINE */
